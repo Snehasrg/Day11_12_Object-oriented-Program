f440f0a124c62c680d0890c2fa19c8a08a5c3618
Snehasrg <sghongade642@gmail.com>
2023-01-07 12:17:02 +0530

    Greeting

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjectOrientedPgm
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Object Oriented programming");
+        }
+    }
+}
