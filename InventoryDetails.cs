b4eabb85e4aa2ad095ae004b3319b60e47436522
Snehasrg <sghongade642@gmail.com>
2023-01-13 21:03:28 +0530

    Details added

---
+++ InventoryDetails.cs
@@ -0,0 +1,32 @@
+﻿namespace ObjectOrientedPgm
+{
+    public class InventoryDetails
+    {
+        public List<TypesOfRice> typesOfRice;
+        public List<TypesOfPuslses> typesOfPulses;
+        public List<TypesOfWheat> typesOfWheat;
+
+    }
+
+    public class TypesOfRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+
+    public class TypesOfPuslses
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+
+    public class TypesOfWheat
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+}
+
