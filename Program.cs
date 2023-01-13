e9011321ba66730e44aac480e7f7afc69bf630a5
Snehasrg <sghongade642@gmail.com>
2023-01-13 21:06:53 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -1,10 +1,56 @@
 ﻿namespace ObjectOrientedPgm
 {
-    public class Program
+    class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Object Oriented programming");
+            Console.WriteLine("***** Welcome to the Object Oriented Program *****");
+
+            string path = @" D:\240\ObjectOrientedProgram\ObjectOrientedPgm\Inventory.json";
+            ReadData obj = new ReadData();
+            var Info = obj.Read(path);
+
+            //Storing the multiple values of Array in List.
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine("Types of Rice is:");
+            Console.WriteLine("--------------------");
+            for (int i = 0; i < Info.typesOfRice.Count; i++)
+            {
+                Console.WriteLine(Info.typesOfRice[i].name);
+                Console.WriteLine(Info.typesOfRice[i].weight);
+                Console.WriteLine(Info.typesOfRice[i].price);
+
+
+            }
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine("Types of Pulses is:");
+            Console.WriteLine("--------------------");
+
+
+            for (int i = 0; i < Info.typesOfPulses.Count; i++)
+            {
+                Console.WriteLine(Info.typesOfPulses[i].name);
+                Console.WriteLine(Info.typesOfPulses[i].weight);
+                Console.WriteLine(Info.typesOfPulses[i].price);
+
+
+
+            }
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine("Types of Wheat is:");
+            Console.WriteLine("--------------------");
+
+            for (int i = 0; i < Info.typesOfWheat.Count; i++)
+            {
+                Console.WriteLine(Info.typesOfWheat[i].name);
+                Console.WriteLine(Info.typesOfWheat[i].weight);
+                Console.WriteLine(Info.typesOfWheat[i].price);
+
+            }
         }
     }
 }
+
